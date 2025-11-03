00491446343efa13c1aa9108d32de9b91c470900
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-11-03 19:59:18 +0900

    생성 시간을 설정하는 코드 작성

--- src/main/java/com/yummap/app/entity/Restaurant.java
+++ src/main/java/com/yummap/app/entity/Restaurant.java
@@ -1,12 +1,11 @@
 package com.yummap.app.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -18,9 +17,19 @@ public class Restaurant {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+    private String area;
+    private String memo;
+    private double lat; // 위도
+    private double lng; // 경도
+    private LocalDateTime dateSaved;
 
-
-
-
+    // 생성시간 설정
+    @PrePersist
+    protected void onCreate() {
+        if (dateSaved == null) {
+            dateSaved = LocalDateTime.now();
+        }
+    }
 }
 
