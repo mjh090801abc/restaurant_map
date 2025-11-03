064606c746cd8f9713f9967f65e254caf327ca41
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-11-03 19:55:29 +0900

    id 만들어주기

---
+++ src/main/java/com/yummap/app/entity/Restaurant.java
@@ -0,0 +1,26 @@
+package com.yummap.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+
+
+
+}
+
