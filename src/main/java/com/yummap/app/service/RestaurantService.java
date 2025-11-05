bfa3514873390b28da6cb652f761ccb075dc71b3
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-11-05 17:13:13 +0900

    RestaurantRepository를 Service에 주입

---
+++ src/main/java/com/yummap/app/service/RestaurantService.java
@@ -0,0 +1,12 @@
+package com.yummap.app.service;
+
+import com.yummap.app.repository.RestaurantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
+}
