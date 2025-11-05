0e97c6ebf9d40f71a672c831666590c5e5ee0f6f
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-11-05 17:04:30 +0900

    @RequestMapping("/api/restaurants")로 controller가 처리하는 엔드포인트 경로를 /api/restaurants로 맞춤

--- src/main/java/com/yummap/app/controller/RestaurantController.java
+++ src/main/java/com/yummap/app/controller/RestaurantController.java
@@ -1,7 +1,14 @@
 package com.yummap.app.controller;
 
+import com.yummap.app.repository.RestaurantRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/restaurants")
 public class RestaurantController {
+
+
 }
