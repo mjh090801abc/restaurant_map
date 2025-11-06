6bd8d55df77c3d341d93466d6b103f2c489cfb77
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-11-06 12:20:13 +0900

    맛집 저장을 위해서 dto 정보 불러오기

--- src/main/java/com/yummap/app/service/RestaurantService.java
+++ src/main/java/com/yummap/app/service/RestaurantService.java
@@ -1,5 +1,6 @@
 package com.yummap.app.service;
 
+import com.yummap.app.dto.RestaurantRequestDto;
 import com.yummap.app.entity.Restaurant;
 import com.yummap.app.repository.RestaurantRepository;
 // import jakarta.transaction.Transactional; 이 import문은 readOnly를 지원하지 않음
@@ -20,15 +21,25 @@ public class RestaurantService {
 
     // 1. 모든 맛집 조회
     @Transactional(readOnly = true)
-    public List<Restaurant> findAllRestaurants() {
-        // Repository의 기본 기능을 호출하여 DB에서 모든 데이터를 가져옵니다.
+    public List<Restaurant> findAllRestaurants() { // List<Restaurant> : List<> 결과는 0 ~ 여러 객채를 담음
+        // Repository의 기본 기능을 호출하여 DB에서 모든 데이터를 가져오기
         return restaurantRepository.findAll();
     }
 
     // 2. 특정 맛집 ID로 조회
     @Transactional(readOnly = true)
-    public Optional<Restaurant> findRestaurantById(Long id) {
-        // Optional<T>는 값이 존재할 수도, 없을 수도 있는 객체를 안전하게 다루게 해줍니다.
+    public Optional<Restaurant> findRestaurantById(Long id) { // Optional<Restaurant> : Optional<> 고유 번호(id) 같은것으로 구분하여 객체를 담음 (결과는 0, 1개)
+        // Optional<T>는 값이 존재할 수도, 없을 수도 있는 객체를 안전하게 다루게 해줌
         return restaurantRepository.findById(id);
     }
+
+    // 3. 맛집 저장 (생성)
+    @Transactional
+    public Restaurant saveNewRestaurant(RestaurantRequestDto dto) {
+        Restaurant restaurant = new Restaurant();
+
+
+    }
+
+
 }
