ae245a95422fdfa59f1974cad56e504efce1b442
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-11-10 19:34:49 +0900

    getmapping으로 레시피를 리스트형태로 한번에 담아오기

--- src/main/java/com/yummap/app/controller/RestaurantController.java
+++ src/main/java/com/yummap/app/controller/RestaurantController.java
@@ -1,14 +1,26 @@
 package com.yummap.app.controller;
 
+import com.yummap.app.entity.Restaurant;
 import com.yummap.app.repository.RestaurantRepository;
+import com.yummap.app.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/restaurants")
 public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    @GetMapping
+    public List<Restaurant> getAllRestaurants() {
+        // Service 계층의 비즈니스 로직 호출
+    }
 
 
 }
