22050bb4a742d785422c3dcd55cb912c721f2e86
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-11-10 19:37:23 +0900

    service에 있는 리스트 안의 모든 값 리턴

--- src/main/java/com/yummap/app/controller/RestaurantController.java
+++ src/main/java/com/yummap/app/controller/RestaurantController.java
@@ -20,6 +20,8 @@ public class RestaurantController {
     @GetMapping
     public List<Restaurant> getAllRestaurants() {
         // Service 계층의 비즈니스 로직 호출
+        // 반환된 List<Restaurant> 객체는 @RestController에 의해 JSON 배열로 변환되어 클라이언트에 응답
+        return restaurantService.findAllRestaurants();
     }
 
 
