2502d37c0184e31b0bfc19eecc385a5175093a35
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-11-10 19:53:47 +0900

    URL 경로의 {id} 값을 추출하여 Long 타입의 id 변수에 대입

--- src/main/java/com/yummap/app/controller/RestaurantController.java
+++ src/main/java/com/yummap/app/controller/RestaurantController.java
@@ -4,9 +4,9 @@ import com.yummap.app.entity.Restaurant;
 import com.yummap.app.repository.RestaurantRepository;
 import com.yummap.app.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,11 +17,19 @@ public class RestaurantController {
 
     private final RestaurantService restaurantService;
 
+    // 맛집 조회
     @GetMapping
     public List<Restaurant> getAllRestaurants() {
         // Service 계층의 비즈니스 로직 호출
         // 반환된 List<Restaurant> 객체는 @RestController에 의해 JSON 배열로 변환되어 클라이언트에 응답
         return restaurantService.findAllRestaurants();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Restaurant> getRestaurant(@PathVariable Long id) {
+        try {
+
+        }
     }
 
 
